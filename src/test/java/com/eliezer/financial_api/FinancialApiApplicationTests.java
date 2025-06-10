09f63b7cda96c2c73c06c4affda09c177d9d5c02
eliezer lobaton <eliezerlobaton39@gmail.com>
2025-06-10 01:22:53 -0300

    first commit

---
+++ src/test/java/com/eliezer/financial_api/FinancialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliezer.financial_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
