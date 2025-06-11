4edc48c24f6e129d06d3de644e2f187c39eed646
eliezerlobaton <eliezerlobaton39@gmail.com>
2025-06-11 02:54:25 -0300

    migration uuid extension

---
+++ src/main/resources/db/migration/V20250611021743__create_ossp.sql
@@ -0,0 +1,3 @@
+-- V20250611021743__create_ossp.sql generated in /home/chiguiro/Workspace/proyectos/financial-app/financial_api/src/main/resources/db/migration
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
